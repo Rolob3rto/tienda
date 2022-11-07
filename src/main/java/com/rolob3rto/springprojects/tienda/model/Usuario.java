bd5e2f1a34a9b4b8c02b6f90e6b9b5ca4dfda08e
RobertoRuizRuiz <robertooli98@gmail.com>
2022-11-07 10:13:48 +0100

    clase Usuario

---
+++ src/main/java/com/rolob3rto/springprojects/tienda/model/Usuario.java
@@ -0,0 +1,53 @@
+package com.rolob3rto.springprojects.tienda.model;
+
+public class Usuario {
+    String nombre;
+    String contrasena;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String contrasena) {
+        this.nombre = nombre;
+        this.contrasena = contrasena;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getContrasena() {
+        return contrasena;
+    }
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Usuario other = (Usuario) obj;
+        if (nombre == null) {
+            if (other.nombre != null)
+                return false;
+        } else if (!nombre.equals(other.nombre))
+            return false;
+        return true;
+    }
+
+    
+}
+
