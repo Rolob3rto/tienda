4635d068b078e14ccf4ec53008f443cbcd220f98
Rolob3rto <robertooli98@gmail.com>
2022-12-02 14:24:48 +0100

    clases de detallePedido y Pedido completas

--- src/main/java/com/rolob3rto/springprojects/tienda/model/DetallePedido.java
+++ src/main/java/com/rolob3rto/springprojects/tienda/model/DetallePedido.java
@@ -7,4 +7,63 @@ public class DetallePedido {
     private Producto producto;
 
     private int cantidad;
+
+    
+
+    public DetallePedido(int codigo, Producto producto, int cantidad) {
+        this.codigo = codigo;
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
+
+    public DetallePedido() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + codigo;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DetallePedido other = (DetallePedido) obj;
+        if (codigo != other.codigo)
+            return false;
+        return true;
+    }
+
+    
 }
--- src/main/java/com/rolob3rto/springprojects/tienda/model/Pedido.java
+++ src/main/java/com/rolob3rto/springprojects/tienda/model/Pedido.java
@@ -14,4 +14,80 @@ public class Pedido {
     private Date fecha;
 
     private float total;
+    
+    public Pedido(int codigo, Cliente cliente, List<DetallePedido> detallePedidos, Date fecha, float total) {
+        this.codigo = codigo;
+        this.cliente = cliente;
+        this.detallePedidos = detallePedidos;
+        this.fecha = fecha;
+        this.total = total;
+    }
+
+    public Pedido() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<DetallePedido> getDetallePedidos() {
+        return detallePedidos;
+    }
+
+    public void setDetallePedidos(List<DetallePedido> detallePedidos) {
+        this.detallePedidos = detallePedidos;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public float getTotal() {
+        return total;
+    }
+
+    public void setTotal(float total) {
+        this.total = total;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + codigo;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pedido other = (Pedido) obj;
+        if (codigo != other.codigo)
+            return false;
+        return true;
+    }
+
+    
+    
 }
